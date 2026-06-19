9769332944a4d01ca14360f022db1e425c98f9e5
ishiyama <hiroyuki293@icloud.com>
2026-06-19 11:12:53 +0900

    rhf、zod追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Form from './components/Form'
 import { useTodos } from './hooks/useTodos'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
@@ -9,24 +10,27 @@ function App() {
   const { todos, addTodo, toggleTodo, deleteTodo, remainingCount } = useTodos()
 
   return (
-    <div className="app">
-      <section>
-        <h1>型付き Todo アプリ</h1>
-        <TodoForm onAdd={addTodo} />
-        <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
-        <p>残り: {remainingCount} 件</p>
-      </section>
+    <>
 
-      <section>
-        <h2>ユーザー一覧</h2>
-        <UserList />
-      </section>
+    <Form/>
 
-      <section>
-        <h2>投稿一覧</h2>
-        <PostList />
-      </section>
-    </div>
+       <section>
+         <h1>型付き Todo アプリ</h1>
+         <TodoForm onAdd={addTodo} />
+         <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+         <p>残り: {remainingCount} 件</p>
+       </section>
+
+       <section>
+         <h2>ユーザー一覧</h2>
+         <UserList />
+       </section>
+
+       <section>
+         <h2>投稿一覧</h2>
+         <PostList />
+       </section>
+    </>
   )
 }
 
